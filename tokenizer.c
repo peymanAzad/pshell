8342494d8840a6d235f9f4bc7b0342ad20f24ba6
peymanazad <azad.peyman75@gmail.com>
2026-07-04 17:35:13 +0400

    improve tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -4,6 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_to_char(char *tokenval, char *input, char ch, int max_len) {
+    int wcount;
+    char c;
+    for (wcount = 0; (c = *input++) != '\0' && c != EOF && c != ch; ++wcount) {
+        if (wcount < max_len)
+            tokenval[wcount] = c;
+    }
+    tokenval[wcount] = '\0';
+    if (c == ch)
+        ++wcount;
+    return wcount;
+}
+
 int tokenize(Token *result, char *input, size_t count) {
     char c;
     size_t cursor = 0;
@@ -14,48 +27,63 @@ int tokenize(Token *result, char *input, size_t count) {
         switch (c) {
         case '\n':
             result[cursor++] = (Token){.type = newline, .value = NULL};
+            ++tcount;
             break;
 
         case '|':
             result[cursor++] = (Token){.type = pipe, .value = NULL};
+            ++tcount;
             break;
 
         case '<':
             result[cursor++] = (Token){.type = lt, .value = NULL};
+            ++tcount;
             break;
 
         case '>':
-            if (*(input + 1) == '>') {
+            if (*(input) == '>') {
                 result[cursor++] = (Token){.type = dgt, .value = NULL};
                 ++input;
             } else
                 result[cursor++] = (Token){.type = gt, .value = NULL};
+            ++tcount;
             break;
 
         case '&':
-            if (*(input + 1) == '&') {
+            if (*(input) == '&') {
                 result[cursor++] = (Token){.type = andd, .value = NULL};
+                ++tcount;
                 ++input;
             }
             break;
 
         case '\\':
             result[cursor++] = (Token){.type = backslash, .value = NULL};
+            ++tcount;
             break;
 
         case '\'':
             tokenval = (char *)malloc(MAX_WORD_LEN + 1);
-            for (wcount = 0; (c = *input++) != '\0' && c != EOF && c != '\'';
-                 ++wcount) {
-                if (wcount < MAX_WORD_LEN)
-                    tokenval[wcount] = c;
+            wcount = get_to_char(tokenval, input, '\'', MAX_WORD_LEN);
+            if (wcount == 0) {
+                free(tokenval);
+                tokenval = NULL;
             }
-            tokenval[wcount] = '\0';
+            input += wcount;
             result[cursor++] = (Token){.type = qoute, .value = tokenval};
+            ++tcount;
             break;
 
         case '"':
-            result[cursor++] = (Token){.type = dqoute, .value = NULL};
+            tokenval = (char *)malloc(MAX_WORD_LEN + 1);
+            wcount = get_to_char(tokenval, input, '"', MAX_WORD_LEN);
+            if (wcount == 0) {
+                free(tokenval);
+                tokenval = NULL;
+            }
+            input += wcount;
+            result[cursor++] = (Token){.type = dqoute, .value = tokenval};
+            ++tcount;
             break;
 
         default:
@@ -67,12 +95,19 @@ int tokenize(Token *result, char *input, size_t count) {
             do {
                 if (wcount < MAX_WORD_LEN)
                     tokenval[wcount++] = c;
+                if (c == '\'' || c == '"') {
+                    int qcount = get_to_char(tokenval + wcount, input, c,
+                                             MAX_WORD_LEN - wcount - 1);
+                    input += qcount;
+                    wcount += qcount;
+                    tokenval[wcount - 1] = c;
+                }
             } while ((c = *input++) != '\0' && c != EOF && !isspace(c));
             tokenval[wcount] = '\0';
             result[cursor++] = (Token){.type = word, .value = tokenval};
+            ++tcount;
             --input;
         }
-        ++tcount;
     }
     return tcount;
 }

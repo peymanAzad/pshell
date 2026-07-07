3a3eae3567119a90e02ffb7a3905ac428b0e55bd
peymanazad <azad.peyman75@gmail.com>
2026-07-07 12:21:23 +0400

    add parser types to the header file

--- pshell.h
+++ pshell.h
@@ -6,6 +6,22 @@
 #define MAX_WORD_LEN 16
 #define BUFF_SIZE 16
 
+/*====================================================
+ * Buffer
+ *====================================================*/
+typedef struct Buffer {
+    char *data;
+    size_t len;
+    size_t cap;
+} Buffer;
+
+int pushbuf(Buffer *buff, char *data);
+int pushcbuf(Buffer *buff, char c);
+Buffer *allocbuf();
+
+/*====================================================
+ * Tokenizer
+ *====================================================*/
 typedef enum TokenType {
     word,
     gt,
@@ -16,23 +32,28 @@ typedef enum TokenType {
     newline
 } TokenType;
 
-typedef struct Buffer {
-    char *data;
-    size_t len;
-    size_t cap;
-} Buffer;
-
 typedef struct Token {
     TokenType type;
     Buffer *value;
 } Token;
 
-int pushbuf(Buffer *buff, char *data);
-int pushcbuf(Buffer *buff, char c);
-Buffer *allocbuf();
-
 size_t next_token(Token *token, char *input, size_t cursor);
 int tokenize_all(Token *tokens, char *input);
 
 
+/*====================================================
+ * Parser
+ *====================================================*/
+typedef enum SyntaxType {
+    script, statement, command, assignment, andor, pipeline
+} SyntaxType;
+
+typedef struct SyntaxNode {
+    SyntaxType type;
+    Buffer *value;
+    struct SyntaxNode *suffixes;
+    struct SyntaxNode *prefixes;
+    struct SyntaxNode *redirects;
+    struct SyntaxNode *next;
+} SyntaxNode;
 #endif
